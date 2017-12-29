67b1228e08ffb4ed07004118c786544475be6f95
Tyler Romero <tyler.alexander.romero@gmail.com>
2017-12-29 15:06:09 -0600

    Config

---
+++ jekyll.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Install Jekyll and Bundler gems through RubyGems
+# gem install jekyll bundler
+
+# Create a new Jekyll site at ./myblog
+# jekyll new .
+
+# jekyll build
+# => The current folder will be generated into ./_site
+
+# jekyll build --watch
+# => The current folder will be generated into ./_site,
+#    watched for changes, and regenerated automatically.
