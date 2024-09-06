3379f3771c398c5c17d16ff2e9ea1088f7ce4856
Tyler Romero <tyler.alexander.romero@gmail.com>
2024-09-06 10:07:06 -0700

    Better mobile formatting (#17)

--- .eleventy.js
+++ .eleventy.js
@@ -1,50 +1,49 @@
 const CleanCSS = require("clean-css");
 const { DateTime } = require("luxon");
-const tufteMdWrapper = require('./util/tufteMdWrapper');
+const tufteMdWrapper = require("./util/tufteMdWrapper");
 
-module.exports = function(eleventyConfig) {
-    // Copy `src/assets` to `_site/assets`
-    eleventyConfig.addPassthroughCopy("src/assets");
+module.exports = function (eleventyConfig) {
+  // Copy `src/assets` to `_site/assets`
+  eleventyConfig.addPassthroughCopy("src/assets");
 
-    // Copy some more files to `_site`
-    eleventyConfig.addPassthroughCopy("src/CNAME");
-    eleventyConfig.addPassthroughCopy("src/robots.txt");
+  // Copy some more files to `_site`
+  eleventyConfig.addPassthroughCopy("src/CNAME");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
 
+  // Asset Watch Targets
+  eleventyConfig.addWatchTarget("./src/assets");
 
-    // Asset Watch Targets
-	eleventyConfig.addWatchTarget('./src/assets')
+  /* Markdown Configuration */
+  let options = {
+    react: false,
+  };
 
-    /* Markdown Configuration */
-	let options = {
-		react: false,
-    };
+  // Markdown
+  eleventyConfig.setLibrary("md", tufteMdWrapper);
+  eleventyConfig.addFilter("markdown", tufteMdWrapper.render);
+  eleventyConfig.addFilter("markdownInline", tufteMdWrapper.renderInline);
 
-	// Markdown
-	eleventyConfig.setLibrary("md", tufteMdWrapper)
-    eleventyConfig.addFilter("markdown", tufteMdWrapper.render)
-    eleventyConfig.addFilter("markdownInline", tufteMdWrapper.renderInline)
+  // Date stuff
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`); // useful for copyright
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toFormat("MMMM yyyy");
+  });
 
-    // Date stuff
-    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);  // useful for copyright
-    eleventyConfig.addFilter("postDate", (dateObj) => {
-        return DateTime.fromJSDate(dateObj).toFormat('MMMM yyyy');
-    });
+  // CSS Minification
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
 
-    // CSS Minification
-    eleventyConfig.addFilter("cssmin", function (code) {
-		return new CleanCSS({}).minify(code).styles;
-	});
-
-    // Set custom directories for input, output, includes, and data
-    return {
-        // When a passthrough file is modified, rebuild the pages:
-        passthroughFileCopy: true,
-        dir: {
-            input: "src",
-            includes: "_includes",
-            layouts: "_layouts",
-            data: "_data",
-            output: "_site",
-        }
-    };
+  // Set custom directories for input, output, includes, and data
+  return {
+    // When a passthrough file is modified, rebuild the pages:
+    passthroughFileCopy: true,
+    dir: {
+      input: "src",
+      includes: "_includes",
+      layouts: "_layouts",
+      data: "_data",
+      output: "_site",
+    },
+  };
 };
--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+.PHONY: help serve build format
+
 help:
 	@echo "Please use \`make <target>' where <target> is one of"
 	@echo "  serve       to run the site locally (with auto-reload)"
@@ -8,3 +10,9 @@ serve:
 
 build:
 	@npx @11ty/eleventy
+
+format:
+	# TODO: format for njk files
+	@npx prettier --write "src/**/*.{html,css,js}"
+	@npx prettier --write "util/tufteMdWrapper.js"
+	@npx prettier --write ".eleventy.js"
